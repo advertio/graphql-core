3ab4fbb9f2d89db2cec778b1a6748edfab32e786
tofran <franciscopcmarques@gmail.com>
2018-09-12 17:32:20 +0100

    GraphQLLocatedError: process non outgoing errors
    Co-authored-by: António Maltsev Santos <antonio.maltsev@gmail.com>

--- graphql/error/located_error.py
+++ graphql/error/located_error.py
@@ -1,18 +1,48 @@
+import logging
 import sys
 
 from .base import GraphQLError
 
 __all__ = ['GraphQLLocatedError']
 
+DEFAULT_OUTGOING_ERROR_STRING = "Error processing request."
+
+logger = logging.getLogger(__name__)
+
 
 class GraphQLLocatedError(GraphQLError):
 
     def __init__(self, nodes, original_error=None):
         if original_error:
-            try:
-                message = str(original_error)
-            except UnicodeEncodeError:
-                message = original_error.message.encode('utf-8')
+
+            if getattr(original_error, "outgoing", False):
+                level = logging.INFO
+                exc_info = False
+                try:
+                    message = str(original_error)
+                except UnicodeEncodeError:
+                    message = original_error.message.encode('utf-8')
+
+            else:
+                level = logging.ERROR
+                exc_info = sys.exc_info()
+                if exc_info[0] is None:
+                    exc_info = False
+
+                message = DEFAULT_OUTGOING_ERROR_STRING
+
+            logger.log(
+                level,
+                "%s caught: %s",
+                type(original_error).__name__,
+                str(original_error),
+
+                extra={
+                    "nodes": repr(nodes),
+                },
+                exc_info=exc_info,
+            )
+
         else:
             message = 'An unknown error occurred.'
 
